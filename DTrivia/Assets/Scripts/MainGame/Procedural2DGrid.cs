91ec552a006b49418e68976974251e2ee752db6c
Christian Andrés Fernández García <kaozgaia@hotmail.com>
2016-08-30 15:56:28 -0500

    Se hace grid procedural y se agregan carpetas
    Se hace grid procedural

---
+++ DTrivia/Assets/Scripts/MainGame/Procedural2DGrid.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Procedural2DGrid : MonoBehaviour {
+    [SerializeField]
+    private int rowSize, columnSize;
+
+    public RectTransform panelRow;
+    public GameObject gridCell;
+
+    public Transform Grid;
+
+    void Init()
+    {
+        rowSize = 3;
+        columnSize = 4;
+    }
+
+    void ClearGrid()
+    {
+        for (int i = 0; i < Grid.childCount; i++)
+        {
+            Destroy(Grid.GetChild(i).gameObject);
+        }
+    }
+
+    public void GenerateGrid()
+    {
+        ClearGrid();
+        Init();
+        GameObject cellInputField;
+        RectTransform rowParent;
+        for (int rowIndex = 0; rowIndex < rowSize; rowIndex++)
+        {
+            rowParent = (RectTransform)Instantiate(panelRow);
+            rowParent.transform.SetParent(Grid);
+            rowParent.transform.localScale = Vector3.one;
+            for (int colIndex = 0; colIndex < columnSize; colIndex++)
+            {
+                cellInputField = (GameObject)Instantiate(gridCell);
+                cellInputField.transform.SetParent(rowParent);
+                cellInputField.GetComponent<Transform>().localScale = Vector3.one;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        GenerateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
